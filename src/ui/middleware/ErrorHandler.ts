94baf00f4f518616daf0091fb69e2aabe3fd807c
Fernando Mercado <fermercado@live.com>
2024-03-06 13:55:05 -0300

    feat: error handler

---
+++ src/ui/middleware/ErrorHandler.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { ApplicationError } from '../../shared/errors/ApplicationError';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ApplicationError) {
+    return res.status(err.statusCode).json({
+      error: {
+        message: err.message,
+        details: err.errors || [],
+      },
+    });
+  }
+
+  console.error('Unhandled Error:', err);
+  return res.status(500).json({
+    error: {
+      message: 'Internal Server Error',
+      details: [],
+    },
+  });
+};
