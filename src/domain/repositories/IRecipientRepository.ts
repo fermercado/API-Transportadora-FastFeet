2eb864fb952342fe8a27d20bc8cb195c664c199e
Fernando Mercado <fermercado@live.com>
2024-03-06 13:48:13 -0300

    fix: update Irecipient repository

--- src/domain/repositories/IRecipientRepository.ts
+++ src/domain/repositories/IRecipientRepository.ts
@@ -2,9 +2,13 @@ import { Recipient } from '../entities/Recipient';
 
 export interface IRecipientRepository {
   create(recipientData: Partial<Recipient>): Promise<Recipient>;
+  update(id: string, recipientData: Partial<Recipient>): Promise<Recipient>;
   save(recipient: Recipient): Promise<Recipient>;
   remove(recipient: Recipient): Promise<void>;
   findById(id: string): Promise<Recipient | undefined>;
+  findByEmail(email: string): Promise<Recipient | undefined>;
+  findByCpf(cpf: string): Promise<Recipient | undefined>;
   findByZipCode(zipCode: string): Promise<Recipient | undefined>;
+  findAll(): Promise<Recipient[]>;
   find(): Promise<Recipient[]>;
 }
