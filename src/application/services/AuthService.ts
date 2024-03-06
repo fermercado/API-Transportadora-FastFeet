f319e13ba86a5f6a70f430a63d823c880951e30c
Fernando Mercado <fermercado@live.com>
2024-03-06 13:54:27 -0300

    fix: update AuthService

--- src/application/services/AuthService.ts
+++ src/application/services/AuthService.ts
@@ -1,27 +1,39 @@
-import jwt, { Secret } from 'jsonwebtoken';
+import { AuthResult, UserDetails } from '../interfaces/IAuthService';
 import { injectable } from 'tsyringe';
+import jwt, { Secret } from 'jsonwebtoken';
 import { UserService } from './UserService';
 
 const JWT_SECRET: Secret = process.env.JWT_SECRET || '';
 
 @injectable()
 export class AuthService {
-  private userService: UserService;
+  constructor(private userService: UserService) {}
 
-  constructor(userService: UserService) {
-    this.userService = userService;
-  }
-
-  async authenticate(cpf: string, password: string): Promise<string | null> {
+  async authenticate(
+    cpf: string,
+    password: string,
+  ): Promise<AuthResult | null> {
     const user = await this.userService.validateUser(cpf, password);
 
     if (!user) {
       return null;
     }
 
-    const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
-      expiresIn: '1h',
+    const token = jwt.sign({ userId: user.id, role: user.role }, JWT_SECRET, {
+      expiresIn: '24h',
     });
-    return token;
+
+    const userDetails: UserDetails = {
+      name: user.firstName + ' ' + user.lastName,
+      email: user.email,
+      role: user.role,
+    };
+
+    const authResult: AuthResult = {
+      token,
+      user: userDetails,
+    };
+
+    return authResult;
   }
 }
