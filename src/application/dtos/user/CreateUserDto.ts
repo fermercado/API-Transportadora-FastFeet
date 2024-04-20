cc0a104f60e7ad219bb072c53b8117318a4dd111
Fernando Mercado <fermercado@live.com>
2024-04-20 12:22:03 -0300

    fix: correction create user dto

--- src/application/dtos/user/CreateUserDto.ts
+++ src/application/dtos/user/CreateUserDto.ts
@@ -1,8 +1,10 @@
+import { UserRole } from '../../../domain/enums/UserRole';
 export interface CreateUserDto {
   cpf: string;
   email: string;
   password: string;
   confirmPassword: string;
-  role: 'admin' | 'deliveryman';
-  name: string;
+  role: UserRole;
+  lastName: string;
+  firstName: string;
 }
