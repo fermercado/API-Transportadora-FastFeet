5c4a888f4e0e94e039ae1df60308b4b22fd2e99d
Fernando Mercado <fermercado@live.com>
2024-04-23 16:47:54 -0300

    refactor: remove archives

--- src/domain/repositories/IDeliverymanRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Deliveryman } from '../entities/Deliveryman';
-
-export interface IDeliverymanRepository {
-  create(deliverymanData: Partial<Deliveryman>): Promise<Deliveryman>;
-  save(deliveryman: Deliveryman): Promise<Deliveryman>;
-  remove(deliveryman: Deliveryman): Promise<void>;
-  findById(id: string): Promise<Deliveryman | undefined>;
-  find(): Promise<Deliveryman[]>;
-}
