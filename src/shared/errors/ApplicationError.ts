4b4c4a8eb42f14b8a2fcbef5a729d36930e01e4f
Fernando Mercado <fermercado@live.com>
2024-04-25 14:57:00 -0300

    refactor: ApplicationError

--- src/shared/errors/ApplicationError.ts
+++ src/shared/errors/ApplicationError.ts
@@ -1,20 +1,40 @@
+import { ErrorDetail } from '../../@types/error-types';
+
 export class ApplicationError extends Error {
   public readonly statusCode: number;
   public readonly isOperational: boolean;
-  public readonly errors?: { field: string; message: string }[];
-    details: any;
+  public readonly details?: ErrorDetail[];
 
   constructor(
     message: string,
     statusCode: number = 500,
     isOperational: boolean = true,
-    errors?: { field: string; message: string }[],
+    details?: ErrorDetail[],
   ) {
     super(message);
     Object.setPrototypeOf(this, new.target.prototype);
     this.statusCode = statusCode;
     this.isOperational = isOperational;
-    this.errors = errors;
+    this.details = details;
     Error.captureStackTrace(this, this.constructor);
+  }
+
+  public formatForLogging(): string {
+    let detailString =
+      this.details?.map((d) => `${d.key}: ${d.value}`).join(', ') ||
+      'No details';
+    return `${this.message} - Status: ${this.statusCode} - Details: ${detailString}`;
+  }
+
+  public serializeForResponse(): object {
+    return {
+      error: this.message,
+      statusCode: this.statusCode,
+      details: this.details,
+    };
+  }
+
+  public isOperationalError(): boolean {
+    return this.isOperational;
   }
 }
