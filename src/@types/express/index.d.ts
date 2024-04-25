60be5802b701d1760798b5953c1331b0bae75b51
Fernando Mercado <fermercado@live.com>
2024-04-25 15:04:58 -0300

    refactor: type express

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { JwtPayload } from '../../application/interfaces/IAuthService';
 
 declare global {
   namespace Express {
     interface Request {
       user?: any;
+    }
+  }
+}
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    clientTimeZone?: string;
+  }
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
     }
   }
 }
