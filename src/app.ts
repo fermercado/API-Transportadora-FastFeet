09ae5ea1f69cc89adbc23eb9e53b5305e73abf6e
Fernando Mercado <fermercado@live.com>
2024-04-25 15:36:34 -0300

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,21 @@ import express, { Express } from 'express';
 import userRoutes from './ui/routes/userRoutes';
 import recipientRoutes from './ui/routes/recipientRoutes';
 import orderRoutes from './ui/routes/orderRoutes';
-import deliverymanRoutes from './ui/routes/deliverymanRoutes';
+import deliveryRoutes from './ui/routes/deliveryRoutes';
 import authRoutes from './ui/routes/authRoutes';
+import { errorHandler } from './ui/middleware/ErrorHandler';
 
 export function createApp(): Express {
   const app = express();
   app.use(express.json());
 
+  app.use(authRoutes);
   app.use(userRoutes);
   app.use(recipientRoutes);
   app.use(orderRoutes);
-  app.use(deliverymanRoutes);
-  app.use(authRoutes);
+  app.use(deliveryRoutes);
+
+  app.use(errorHandler);
 
   return app;
 }
