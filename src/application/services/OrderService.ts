e4ed2ac064eddc9ac196540f3975525da22ad423
Fernando Mercado <fermercado@live.com>
2024-05-07 14:52:27 -0300

    refactor: refactor order service

--- src/application/services/OrderService.ts
+++ src/application/services/OrderService.ts
@@ -3,10 +3,15 @@ import { Order } from '../../domain/entities/Order';
 import { IOrderRepository } from '../../domain/repositories/IOrderRepository';
 import { CreateOrderDto } from '../dtos/order/CreateOrderDto';
 import { UpdateOrderDto } from '../dtos/order/UpdateOrderDto';
-import { OrderValidationService } from '../../application/validation/OrderValidationService';
+import { OrderValidationService } from '../validation/OrderValidationService';
 import { OrderStatus } from '../../domain/enums/OrderStatus';
-import { DeliveryNotificationService } from './DeliveryNotificationService';
 import ImageUploadService from '../../infrastructure/service/ImageUploadService';
+import { OrderResponseDto } from '../dtos/order/ResponseOrderDto';
+import { OrderMapper } from '../mappers/OrderMapper';
+import { OrderFilter } from '../../domain/interface/OrderFilter';
+import { TrackingCodeService } from '../services/TrackingCodeService';
+import { NotificationService } from '../services/NotificationService';
+import { UserRole } from '../../domain/enums/UserRole';
 
 @injectable()
 export class OrderService {
@@ -14,42 +19,16 @@ export class OrderService {
     @inject('IOrderRepository') private orderRepository: IOrderRepository,
     @inject('OrderValidationService')
     private orderValidationService: OrderValidationService,
-    @inject('DeliveryNotificationService')
-    private deliveryNotificationService: DeliveryNotificationService,
+    @inject('OrderMapper') private orderMapper: OrderMapper,
+    @inject('TrackingCodeService')
+    private trackingCodeService: TrackingCodeService,
+    @inject('NotificationService')
+    private notificationService: NotificationService,
   ) {}
 
-  private async notifyStatusChange(order: Order) {
-    if (!order.recipient.email) return;
-
-    try {
-      await this.deliveryNotificationService.sendStatusUpdateEmail(
-        order.recipient.email,
-        order.recipient.firstName,
-        order.status,
-      );
-    } catch (error) {
-      console.error('Failed to send status notification:', error);
-      throw error;
-    }
-  }
-
-  private generateTrackingCode(
-    carrierName: string,
-    recipientState: string,
-  ): string {
-    const carrierInitials = carrierName
-      .split(' ')
-      .map((word) => word[0])
-      .join('')
-      .toUpperCase()
-      .substring(0, 2);
-    const uniqueNumber = Math.floor(Math.random() * 1e9)
-      .toString()
-      .padStart(9, '0');
-    return `${carrierInitials}${uniqueNumber}${recipientState.toUpperCase()}`;
-  }
-
-  public async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
+  public async createOrder(
+    createOrderDto: CreateOrderDto,
+  ): Promise<OrderResponseDto> {
     const order = new Order();
     order.recipient = await this.orderValidationService.validateRecipient(
       createOrderDto.recipientId,
@@ -57,27 +36,27 @@ export class OrderService {
     order.deliveryman = await this.orderValidationService.validateDeliveryman(
       createOrderDto.deliverymanId,
     );
-    order.trackingCode = this.generateTrackingCode(
+    order.trackingCode = this.trackingCodeService.generateTrackingCode(
       'Fast Feet',
       order.recipient.state,
     );
     order.status = OrderStatus.Pending;
-
     const savedOrder = await this.orderRepository.create(order);
-    await this.notifyStatusChange(order);
-
-    return savedOrder;
+    await this.notifyStatusChange(savedOrder);
+    return this.orderMapper.toDto(savedOrder);
   }
 
   public async updateOrder(
     id: string,
     updateOrderDto: UpdateOrderDto,
-  ): Promise<Order> {
-    const order = await this.orderValidationService.validateOrderUpdate(
-      id,
-      updateOrderDto,
-    );
-    return await this.orderRepository.save(order);
+  ): Promise<OrderResponseDto> {
+    const order =
+      await this.orderValidationService.validateAndUpdateOrderDetails(
+        id,
+        updateOrderDto,
+      );
+    const updatedOrder = await this.orderRepository.save(order);
+    return this.orderMapper.toDto(updatedOrder);
   }
 
   public async deleteOrder(id: string): Promise<void> {
@@ -85,80 +64,120 @@ export class OrderService {
     await this.orderRepository.remove(id);
   }
 
-  public async getOrderById(id: string): Promise<Order | null> {
-    return await this.orderValidationService.validateOrderExistence(id);
+  public async getOrderById(id: string): Promise<OrderResponseDto | null> {
+    const order = await this.orderValidationService.validateOrderExistence(id);
+    if (!order) return null;
+    return this.orderMapper.toDto(order);
   }
 
-  public async listOrders(): Promise<Order[]> {
-    return await this.orderRepository.find();
+  public async listOrders(status?: OrderStatus): Promise<OrderResponseDto[]> {
+    const orders = status
+      ? await this.orderRepository.findByFilter({ status: status })
+      : await this.orderRepository.find();
+    return orders.map(this.orderMapper.toDto);
   }
 
-  async markOrderAsWaiting(orderId: string): Promise<Order> {
+  public async markOrderAsWaiting(
+    orderId: string,
+    _deliverymanId: string,
+    userRole: UserRole,
+  ): Promise<OrderResponseDto> {
     const order = await this.orderValidationService.validateOrderTransition(
       orderId,
       OrderStatus.AwaitingPickup,
+      _deliverymanId,
+      userRole,
     );
     order.status = OrderStatus.AwaitingPickup;
-    await this.orderRepository.save(order);
-    await this.notifyStatusChange(order);
-    return order;
+    order.awaitingPickupAt = new Date();
+    const savedOrder = await this.orderRepository.save(order);
+    await this.notifyStatusChange(savedOrder);
+    return this.orderMapper.toDto(savedOrder);
   }
 
-  async pickupOrder(orderId: string, _deliverymanId: string): Promise<Order> {
+  public async pickupOrder(
+    orderId: string,
+    _deliverymanId: string,
+    userRole: UserRole,
+  ): Promise<OrderResponseDto> {
     const order = await this.orderValidationService.validateOrderTransition(
       orderId,
       OrderStatus.PickedUp,
+      _deliverymanId,
+      userRole,
     );
     order.status = OrderStatus.PickedUp;
-    await this.orderRepository.save(order);
-    await this.notifyStatusChange(order);
-    return order;
+    order.pickedUpAt = new Date();
+    const savedOrder = await this.orderRepository.save(order);
+    await this.notifyStatusChange(savedOrder);
+    return this.orderMapper.toDto(savedOrder);
   }
 
-  async markOrderAsDelivered(
+  public async markOrderAsDelivered(
     orderId: string,
     deliverymanId: string,
     imageFile: Express.Multer.File,
-  ): Promise<Order> {
+  ): Promise<OrderResponseDto> {
     const order = await this.orderValidationService.validateOrderForDelivery(
       orderId,
       deliverymanId,
       imageFile,
     );
-
     const imageUrl = await ImageUploadService.uploadImage(imageFile.path);
     order.deliveryPhoto = imageUrl;
     order.status = OrderStatus.Delivered;
-    await this.orderRepository.save(order);
-    await this.notifyStatusChange(order);
-    return order;
+    order.deliveredAt = new Date();
+    const savedOrder = await this.orderRepository.save(order);
+    await this.notifyStatusChange(savedOrder);
+    return this.orderMapper.toDto(savedOrder);
   }
 
-  async returnOrder(orderId: string, _deliverymanId: string): Promise<Order> {
+  public async returnOrder(
+    orderId: string,
+    _deliverymanId: string,
+    userRole: UserRole,
+  ): Promise<OrderResponseDto> {
     const order = await this.orderValidationService.validateOrderTransition(
       orderId,
       OrderStatus.Returned,
+      _deliverymanId,
+      userRole,
     );
     order.status = OrderStatus.Returned;
-    await this.orderRepository.save(order);
-    await this.notifyStatusChange(order);
-    return order;
+    order.returnedAt = new Date();
+    const savedOrder = await this.orderRepository.save(order);
+    await this.notifyStatusChange(savedOrder);
+    return this.orderMapper.toDto(savedOrder);
   }
 
   public async findDeliveriesForDeliverer(
     deliverymanId: string,
-  ): Promise<Order[]> {
-    return this.orderRepository.findByDeliveryman(deliverymanId);
+    status?: OrderStatus,
+  ): Promise<OrderResponseDto[]> {
+    const filter: OrderFilter = {
+      deliverymanId: deliverymanId,
+      status: status,
+    };
+    const orders = await this.orderRepository.findByFilter(filter);
+    return orders.map(this.orderMapper.toDto);
   }
 
   public async findNearbyDeliveries(
     deliverymanId: string,
     zipCode: string,
-  ): Promise<{ order: Order; distance: string }[]> {
-    return await this.orderValidationService.findNearbyDeliveries(
+  ): Promise<{ order: OrderResponseDto; distance: string }[]> {
+    const deliveries = await this.orderValidationService.findNearbyDeliveries(
       deliverymanId,
       zipCode,
       this.orderRepository,
     );
+    return deliveries.map((delivery) => ({
+      ...delivery,
+      order: this.orderMapper.toDto(delivery.order),
+    }));
+  }
+
+  private async notifyStatusChange(order: Order) {
+    await this.notificationService.notifyStatusChange(order);
   }
 }
