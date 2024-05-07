266ea683f79ee9786fe1190c087a7eaa4f309534
Fernando Mercado <fermercado@live.com>
2024-05-07 14:55:03 -0300

    refactor: order validator

--- src/domain/validators/OrderValidator.ts
+++ src/domain/validators/OrderValidator.ts
@@ -1,23 +1,19 @@
 import { z } from 'zod';
 
-export const createOrderSchema = z.object({
-  recipientId: z.string().uuid({ message: 'Valid recipient ID is required' }),
-  deliverymanId: z.string().uuid().optional(),
-});
+class OrderValidator {
+  static createOrderSchema = z.object({
+    recipientId: z.string().uuid({ message: 'Valid recipient ID is required' }),
+    deliverymanId: z.string().uuid().optional(),
+  });
 
-export const updateOrderSchema = z.object({
-  recipientId: z.string().uuid().optional(),
-  deliverymanId: z.string().uuid().optional(),
-  status: z
-    .enum(['pending', 'awaiting_pickup', 'delivered', 'returned'])
-    .optional(),
-  deliveryPhoto: z.string().optional(),
-});
+  static updateOrderSchema = z
+    .object({
+      recipientId: z.string().uuid(),
+      deliverymanId: z.string().uuid(),
+      status: z.enum(['pending', 'awaiting_pickup', 'delivered', 'returned']),
+      deliveryPhoto: z.string(),
+    })
+    .partial();
+}
 
-export const validateCreateOrder = (data: object) => {
-  return createOrderSchema.parse(data);
-};
-
-export const validateUpdateOrder = (data: object) => {
-  return updateOrderSchema.parse(data);
-};
+export default OrderValidator;
