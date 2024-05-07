0cd351c55501cab7a7161f2abd37013f00d004fe
Fernando Mercado <fermercado@live.com>
2024-05-07 14:54:48 -0300

    refactor: IOrder repository

--- src/domain/repositories/IOrderRepository.ts
+++ src/domain/repositories/IOrderRepository.ts
@@ -1,11 +1,12 @@
 import { Order } from '../../domain/entities/Order';
+import { OrderFilter } from '../../domain/interface/OrderFilter';
 
 export interface IOrderRepository {
   create(orderData: Partial<Order>): Promise<Order>;
   update(id: string, orderData: Partial<Order>): Promise<Order>;
   find(): Promise<Order[]>;
   findById(id: string): Promise<Order | undefined>;
-  findByDeliveryman(deliverymanId: string): Promise<Order[]>;
+  findByFilter(filter: OrderFilter): Promise<Order[]>;
   save(order: Order): Promise<Order>;
   remove(id: string): Promise<void>;
 }
