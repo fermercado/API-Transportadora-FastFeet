92092d34cd4633c08104c323253c741e3c1e2c52
Fernando Mercado <fermercado@live.com>
2024-05-07 15:13:51 -0300

    refactor: iuser repository

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,10 +1,12 @@
 import { User } from '../entities/User';
+import { UserFilter } from '../../domain/interface/UserFilter';
 
 export interface IUserRepository {
   create(userData: Partial<User>): Promise<User>;
   findById(id: string): Promise<User | undefined>;
   findByCpf(cpf: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
+  findByFilter(filter: UserFilter): Promise<User[]>;
   update(id: string, userData: Partial<User>): Promise<User>;
   findAll(): Promise<User[]>;
   save(user: User): Promise<User>;
