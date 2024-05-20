bbca6f4ba2a6d060db6a2b8e05f614cb3ac7c068
Fernando Mercado <fermercado@live.com>
2024-05-20 15:12:47 -0300

    refactor: refactor auth route

--- src/ui/routes/authRoutes.ts
+++ src/ui/routes/authRoutes.ts
@@ -1,4 +1,4 @@
-import { Router, Express } from 'express';
+import { Router } from 'express';
 import { container } from 'tsyringe';
 import { AuthController } from '../controllers/AuthController';
 
@@ -8,7 +8,3 @@ const authController = container.resolve(AuthController);
 router.post('/api/v1/login', (req, res) => authController.login(req, res));
 
 export default router;
-
-export function setupAuthRoutes(app: Express): void {
-  app.use(router);
-}
