a0ad852eb3b436f21945f149014d85c955f7ef06
Fernando Mercado <fermercado@live.com>
2024-05-20 15:17:41 -0300

    refactor: services dependencies

--- src/infrastructure/dependencyInjection/servicesDependencies.ts
+++ src/infrastructure/dependencyInjection/servicesDependencies.ts
@@ -1,15 +1,32 @@
 import { container } from 'tsyringe';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import AppDataSource from '../../infrastructure/orm/ormconfig';
 import { AuthService } from '../../application/services/AuthService';
+import { UserService } from '../../application/services/UserService';
 import { DeliveryNotificationService } from '../../application/services/DeliveryNotificationService';
 import { ExternalServices } from '../../infrastructure/externalService/ExternalService';
-import { UniqueValidationUtils } from '../shared/utils/uniqueValidationUtils';
+import { UniqueValidationUtils } from '../../application/utils/uniqueValidationUtils';
+import { PasswordHasher } from '../../application/utils/PasswordHasher';
+import { UserValidationService } from '../../application/validation/UserValidationService';
+import { UserMapper } from '../../application/mappers/UserMappers';
+
+const jwtSecret = process.env.JWT_SECRET || 'default_secret';
+
+container.register('JWT_SECRET', { useValue: jwtSecret });
 
 container.registerSingleton('AuthService', AuthService);
+container.registerSingleton('UserService', UserService);
 container.registerSingleton(
   'DeliveryNotificationService',
   DeliveryNotificationService,
 );
 container.registerSingleton('ExternalServices', ExternalServices);
 container.registerSingleton('UniqueValidationUtils', UniqueValidationUtils);
+container.registerSingleton('PasswordHasher', PasswordHasher);
+container.registerSingleton('UserValidationService', UserValidationService);
+container.registerSingleton('UserMapper', UserMapper);
 
-export { container } from 'tsyringe';
+container.registerInstance<DataSource>('DataSource', AppDataSource);
+
+export { container };
