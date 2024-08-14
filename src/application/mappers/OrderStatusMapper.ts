c7341e2054f909dd1989b7360be7c06e93fbd165
Fernando Mercado <fermercado@live.com>
2024-08-14 14:15:44 -0300

    chore: correction of status order

--- src/application/mappers/OrderStatusMapper.ts
+++ src/application/mappers/OrderStatusMapper.ts
@@ -15,56 +15,59 @@ export class OrderStatusMapper {
       statusHistory.push({
         status: OrderStatus.Pending,
         message: translateStatus(OrderStatus.Pending),
-        date: DateUtils.formatToBrazilianDateTime(order.createdAt),
+        date: order.createdAt.toISOString(),
       });
     }
-
     if (order.awaitingPickupAt) {
       statusHistory.push({
         status: OrderStatus.AwaitingPickup,
         message: translateStatus(OrderStatus.AwaitingPickup),
-        date: DateUtils.formatToBrazilianDateTime(order.awaitingPickupAt),
+        date: order.awaitingPickupAt.toISOString(),
       });
     }
-
     if (order.pickedUpAt) {
       statusHistory.push({
         status: OrderStatus.PickedUp,
         message: translateStatus(OrderStatus.PickedUp),
-        date: DateUtils.formatToBrazilianDateTime(order.pickedUpAt),
+        date: order.pickedUpAt.toISOString(),
       });
     }
-
     if (order.deliveredAt) {
       statusHistory.push({
         status: OrderStatus.Delivered,
         message: translateStatus(OrderStatus.Delivered),
-        date: DateUtils.formatToBrazilianDateTime(order.deliveredAt),
+        date: order.deliveredAt.toISOString(),
       });
     }
-
     if (order.returnedAt) {
       statusHistory.push({
         status: OrderStatus.Returned,
         message: translateStatus(OrderStatus.Returned),
-        date: DateUtils.formatToBrazilianDateTime(order.returnedAt),
+        date: order.returnedAt.toISOString(),
       });
     }
 
     statusHistory.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
     );
 
-    const currentStatus = statusHistory[statusHistory.length - 1];
+    const currentStatus = statusHistory[0];
 
-    const filteredStatusHistory = statusHistory.filter(
-      (status) => status !== currentStatus,
-    );
+    const filteredStatusHistory = statusHistory
+      .slice(1)
+      .reverse()
+      .map((status) => ({
+        ...status,
+        date: DateUtils.formatToBrazilianDateTime(new Date(status.date)),
+      }));
 
     return {
       trackingCode: order.trackingCode,
       statusHistory: filteredStatusHistory,
-      currentStatus,
+      currentStatus: {
+        ...currentStatus,
+        date: DateUtils.formatToBrazilianDateTime(new Date(currentStatus.date)),
+      },
     };
   }
 }
